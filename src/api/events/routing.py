9346ea59e32ec9b9a5241448e2b4f3f63fc53329
Stefano Gandolfo <stefanogandolfo@Stefanos-MacBook-Air.local>
2025-04-08 16:09:08 +0200

    Create POST route

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -3,7 +3,7 @@ from .schemas import EventSchema, EventListSchema
 
 router = APIRouter()
 
-
+# GET /api/events
 @router.get("/")
 def read_events() -> EventListSchema:
     return {
@@ -11,7 +11,18 @@ def read_events() -> EventListSchema:
         "count": 3
         }
 
+# SEND DATA HERE
+# create view
+# POST /api/events
+@router.post("/")
+def create_events(data:dict = {}) -> EventSchema:
+    print(data)
+    return {
+            "id": 123,
+        }
 
+
+# GET /api/events/12
 @router.get("/{event_id}")
 def get_event(event_id: int) -> EventSchema:
     return {
