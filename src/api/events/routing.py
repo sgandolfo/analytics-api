61a7ef1398478acbcf170f40dc409a4abea58295
Stefano Gandolfo <stefanogandolfo@Stefanos-MacBook-Air.local>
2025-04-08 15:40:40 +0200

    Create Juypiter notebook to test data types

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -8,3 +8,10 @@ def read_events():
     return {
         "items": [1, 2, 3]
         }
+
+
+@router.get("/{event_id}")
+def get_event(event_id: int):
+    return {
+        "id": event_id
+        }
