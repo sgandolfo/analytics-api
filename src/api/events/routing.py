dda55a239bd89e5245347eda86e7248358d9f5aa
Stefano Gandolfo <stefano.gandolfo@icloud.com>
2025-04-08 23:23:28 +0200

    Create GET and PUT specific event id route

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 
 from api.db.session import get_session
@@ -33,17 +33,30 @@ def create_event(
 
 
 # GET /api/events/12
-@router.get("/{event_id}")
-def get_event(event_id: int) -> EventModel:
-    return {
-        "id": event_id,
-        }
+@router.get("/{event_id}", response_model=EventModel)
+def get_event(event_id: int, session:Session = Depends(get_session)):
+    query = select(EventModel).where(EventModel.id == event_id)
+    result = session.exec(query).first()
+    if not result:
+        raise HTTPException(status_code=404, detail="Event not found")
+    return result
 
-@router.put("/{event_id}")
-def update_event(event_id: int, payload:EventUpdateSchema) -> EventModel:
+@router.put("/{event_id}", response_model=EventModel)
+def update_event(
+    event_id: int,
+    payload:EventUpdateSchema,
+    session:Session = Depends(get_session)):
+
+    query = select(EventModel).where(EventModel.id == event_id)
+    obj = session.exec(query).first()
+    if not obj:
+        raise HTTPException(status_code=404, detail="Event not found")
+
     data = payload.model_dump()
-    print(data)
-    return {
-        "id": event_id,
-        **data
-        }
+    for key, value in data.items():
+        setattr(obj, key, value)
+
+    session.add(obj)
+    session.commit()
+    session.refresh(obj)
+    return obj
